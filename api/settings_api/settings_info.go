82c1328e95ccc897c2473a742a72825a4c2b0aa9
Roy <qq3392313023@163.com>
2023-10-28 17:08:38 +0800

    Result响应封装

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -7,5 +7,9 @@ import (
 
 // SettingsInfoView 视图
 func (SettingsApi) SettingsInfoView(c *gin.Context) {
-	res.ResultFailWithCode(2, c)
+	res.ResultOkWithData(gin.H{
+		"event":    "settings_info",
+		"姓名":     "sb 李磊",
+		"我成功了": "李磊减少十年寿命",
+	}, c)
 }
