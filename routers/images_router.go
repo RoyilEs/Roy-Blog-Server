df050195a9307e8a601140b5d1e1f585865d45c4
Roy <qq3392313023@163.com>
2023-11-13 20:35:21 +0800

    权限更新

--- routers/images_router.go
+++ routers/images_router.go
@@ -2,11 +2,6 @@ package routers
 
 import (
 	"Goblog/api"
-	"Goblog/api/images_api"
-	"Goblog/global"
-	"Goblog/models"
-	"Goblog/models/res"
-	"github.com/gin-gonic/gin"
 )
 
 func (router Group) ImagesRouter() {
@@ -16,26 +11,5 @@ func (router Group) ImagesRouter() {
 	router.GET("images_names", imagesApi.ImageNameListView)
 	router.DELETE("images", imagesApi.ImageRemoveView)
 	//TODO 不知道哪里出的bug 包传递也不行了 ---- 直接匿名函数
-	router.PUT("images", func(c *gin.Context) {
-		var cr images_api.ImageUpdateRequest
-		err := c.ShouldBindJSON(&cr)
-		if err != nil {
-			res.ResultFailWithError(err, &cr, c)
-			return
-		}
-		//拿到ID 进行修改
-		var imageModel models.BannerModel
-		err = global.DB.Take(&imageModel, cr.ID).Error
-		if err != nil {
-			res.ResultFailWithMsg("文件不存在", c)
-			return
-		}
-		//修改
-		err = global.DB.Model(&imageModel).Update("name", cr.Name).Error
-		if err != nil {
-			res.ResultFailWithMsg(err.Error(), c)
-			return
-		}
-		res.ResultOkWithMsg("文件名修改成功", c)
-	})
+	router.PUT("images", imagesApi.ImageUpdateView)
 }
