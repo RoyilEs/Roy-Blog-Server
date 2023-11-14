03f867d39f6f332a90310ed7bb408fbdd55a5cb7
Roy <qq3392313023@163.com>
2023-11-14 22:35:38 +0800

    注销优化 text

--- api/user_api/user_logout.go
+++ api/user_api/user_logout.go
@@ -24,3 +24,8 @@ func (UserApi) LogoutView(c *gin.Context) {
 
 	res.ResultOkWithMsg("注销成功", c)
 }
+
+//注销 记一次笔记 写入server 作为全局
+//其中调用 redis_ser 的 logout针对注销的操作存入redis
+//在中间件中判断是否登录 未登录则返回错误 获得redis中 keys 在utils.InList 判断是否在redis中 --> redis_ser.CheckLogout
+//存在则为注销
